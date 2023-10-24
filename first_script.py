f3a4657645f5fe9adddbc8478154a8ed062feeea
tkhvorova <tkhvorova@ahconn.com>
2023-10-24 15:06:33 +0700

    adding file from previous module

---
+++ first_script.py
@@ -0,0 +1,38 @@
+import time
+
+# webdriver это и есть набор команд для управления браузером
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), 
+options=options)
+
+# команда time.sleep устанавливает паузу в 5 секунд, чтобы мы успели увидеть, что происходит в браузере
+time.sleep(5)
+
+# Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
+driver.get("https://stepik.org/lesson/25969/step/12")
+time.sleep(20)
+
+# Метод find_element позволяет найти нужный элемент на сайте, указав путь к нему. Способы поиска элементов мы обсудим позже
+# Метод принимает в качестве аргументов способ поиска и значение, по которому мы будем искать
+# Ищем поле для ввода текста
+textarea = driver.find_element(By.CSS_SELECTOR, ".textarea")
+
+# Напишем текст ответа в найденное поле
+textarea.send_keys("get()")
+time.sleep(10)
+
+# Найдем кнопку, которая отправляет введенное решение
+submit_button = driver.find_element(By.CSS_SELECTOR, ".submit-submission")
+
+# Скажем драйверу, что нужно нажать на кнопку. После этой команды мы должны увидеть сообщение о правильном ответе
+submit_button.click()
+time.sleep(5)
+
+# После выполнения всех действий мы должны не забыть закрыть окно браузера
+driver.quit()
